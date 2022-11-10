fd27908496c90352b684e80c550b135eced86e27
Serkan Uludag <hsuludag@outlook.com>
2022-11-10 16:34:36 +0300

    faq

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
 import Calendar from './scenes/calendar';
 import Form from './scenes/form';
+import FAQ from './scenes/faq';
 // import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
-// import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
 
 function App() {
@@ -34,11 +34,11 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
               {/*
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
               */}
             </Routes>
