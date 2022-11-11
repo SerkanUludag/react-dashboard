a58b559458ae672eaa67b922456d5622f078f881
Serkan Uludag <hsuludag@outlook.com>
2022-11-11 15:21:08 +0300

    charts

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ import Calendar from './scenes/calendar';
 import Form from './scenes/form';
 import FAQ from './scenes/faq';
 import Bar from './scenes/bar';
-// import Line from './scenes/line';
-// import Pie from './scenes/pie';
-// import Geography from './scenes/geography';
+import Pie from './scenes/pie';
+import Line from './scenes/line';
+import Geography from './scenes/geography';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -36,11 +36,9 @@ function App() {
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
-              {/*
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/geography" element={<Geography />} />
-              */}
             </Routes>
           </main>
         </div>
